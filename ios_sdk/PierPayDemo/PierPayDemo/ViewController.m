746ffcd4b52954ff7fae6445ea0817de3a91c854
zyma <zyma678@gmail.com>
2015-03-03 17:57:42 +0800

    merchant pay by pier demo.

--- ios_sdk/PierPayDemo/PierPayDemo/ViewController.m
+++ ios_sdk/PierPayDemo/PierPayDemo/ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 #import "PierPay.h"
 @interface ViewController () <PayByPierDelegate>
+
+@property (nonatomic, weak) IBOutlet UITextField *phoneLabel;
+@property (nonatomic, weak) IBOutlet UITextField *countryCodeLabel;
+@property (nonatomic, weak) IBOutlet UITextField *merchantIDLabel;
+@property (nonatomic, weak) IBOutlet UITextField *amountLabel;
+@property (nonatomic, weak) IBOutlet UITextField *currencyLabel;
+@property (nonatomic, weak) IBOutlet UITextField *serviewURL;
+
+@property (nonatomic, strong) NSMutableDictionary *merchantParam;
+
 @end
 
 @implementation ViewController
@@ -16,18 +26,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initData];
+    [self initView];
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    [self.phoneLabel becomeFirstResponder];
+}
+
+- (void)viewDidDisappear:(BOOL)animated{
+    [self.phoneLabel resignFirstResponder];
+}
+
+- (void)initData{
+    _merchantParam = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                      @"1879654567", @"phone",
+                      @"US", @"country_code",
+                      @"MC0000000134", @"merchant_id",
+                      @"500", @"amount",
+                      @"USD", @"currency",
+                      @"http://192.168.1.96:8080/pier-merchant/merchant/server/sdk/pay/", @"server_url",nil];
+    
+    [self.phoneLabel setText:[_merchantParam objectForKey:@"phone"]];
+    [self.countryCodeLabel setText:[_merchantParam objectForKey:@"country_code"]];
+    [self.merchantIDLabel setText:[_merchantParam objectForKey:@"merchant_id"]];
+    [self.amountLabel setText:[_merchantParam objectForKey:@"amount"]];
+    [self.currencyLabel setText:[_merchantParam objectForKey:@"currency"]];
+    [self.serviewURL setText:[_merchantParam objectForKey:@"server_url"]];
+}
+
+- (void)initView{
+    
+}
+
+
 - (IBAction)payByPier:(id)sender{
-//    [PierPaySDK test:self.textView.text];
-    NSDictionary *userAttributes = [NSDictionary dictionaryWithObjectsAndKeys:
-                                    @"1879654567", @"phone",
-                                    @"US", @"country_code",
-                                    @"MC0000000134", @"merchant_id",
-                                    @"500", @"amount",
-                                    @"USD", @"currency",
-                                    @"http://192.168.1.96:8080/pier-merchant/merchant/server/sdk/pay/", @"server_url",nil];
-    PierPay *pierpay = [[PierPay alloc] initWith:userAttributes];
+    [self setMerchantParam];
+    PierPay *pierpay = [[PierPay alloc] initWith:_merchantParam];
     pierpay.pierDelegate = self;
     [self presentViewController:pierpay animated:YES completion:nil];
 }
@@ -39,6 +74,16 @@
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     [self.view endEditing:YES];
+}
+
+
+- (void)setMerchantParam{
+    [_merchantParam setValue:self.phoneLabel.text forKey:@"phone"];
+    [_merchantParam setValue:self.countryCodeLabel.text forKey:@"phone"];
+    [_merchantParam setValue:self.merchantIDLabel.text forKey:@"phone"];
+    [_merchantParam setValue:self.amountLabel.text forKey:@"phone"];
+    [_merchantParam setValue:self.currencyLabel.text forKey:@"phone"];
+    [_merchantParam setValue:self.serviewURL.text forKey:@"phone"];
 }
 
 /**
