1bd68329aee954194e53ca6133197b2ac846be7f
zyma <zyma678@gmail.com>
2015-03-12 20:04:24 +0800

    credit apply ssn formate.

--- ios_sdk/PierPaySDK/PierPaySDK/PierSiginCells.m
+++ ios_sdk/PierPaySDK/PierPaySDK/PierSiginCells.m
@@ -228,7 +228,7 @@
 }
 
 - (NSString *)getSSN{
-    NSString *ssn = self.ssnLabel.text;
+    NSString *ssn = [self.ssnLabel.text stringByReplacingOccurrencesOfString:@" " withString:@""];
     return ssn;
 }
 
