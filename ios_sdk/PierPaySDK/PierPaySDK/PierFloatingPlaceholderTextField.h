b6154d0cbb8f4a1143cc2360abdb471af2b23ee6
zyma <zyma678@gmail.com>
2015-03-18 18:52:29 +0800

    icon.

--- ios_sdk/PierPaySDK/PierPaySDK/PierFloatingPlaceholderTextField.h
+++ ios_sdk/PierPaySDK/PierPaySDK/PierFloatingPlaceholderTextField.h
@@ -9,7 +9,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "RPFloatingPlaceholderConstants.h"
+#import "RierFloatingPlaceholderConstants.h"
 
 @interface PierFloatingPlaceholderTextField : UITextField
 
