5844e36ef0568f6efb3bbc0214f5aaf737ce6f00
Sathish Nagappan <srn@pierup.com>
2015-05-13 16:18:32 -0700

    change to flask

--- nodejs_sdk/NodeJSMerchant.js
+++ nodejs_sdk/NodeJSMerchant.js
@@ -31,6 +31,6 @@ app.post('/', function(req, res){
 	PierTransaction(req.body, res);
 });
 
-app.listen(8080, function(){
-	console.log('Starting server on port 8080...')
+app.listen(8081, function(){
+	console.log('Starting server on port 8081...')
 });
--- python_sdk/PythonMerchant.py
+++ python_sdk/PythonMerchant.py
@@ -1,44 +1,36 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import urlparse
-import json
+from flask import Flask, request
+app = Flask(__name__)
 
+import json
 from PierSDK import MerchantSDKClient, TransactionConfig
 
-class MerchantHandler(BaseHTTPRequestHandler):
+def pierTransaction(amount, auth_token, currency, id_in_merchant):
+	merchant = MerchantSDKClient("MC0000014895")
+	config = TransactionConfig(
+	    amount, 
+	    "5b52051a-931a-11e4-aad2-0ea81fa3d43c", 
+	    "mk-test-5b52041f-931a-11e4-aad2-0ea81fa3d43c", 
+	    auth_token, 
+	    currency, 
+	    id_in_merchant, 
+	    "dummy Python merchant"
+	)
+	result = merchant.transaction(config)
+	return json.dumps(result.__dict__)
 
-    def pierTransaction(self, amount, auth_token, currency, order_id):
-        merchant = MerchantSDKClient("MC0000014895")
-        config = TransactionConfig(
-            amount, 
-            "5b52051a-931a-11e4-aad2-0ea81fa3d43c", 
-            "mk-test-5b52041f-931a-11e4-aad2-0ea81fa3d43c", 
-            auth_token, 
-            currency, 
-            order_id, 
-            "dummy Python merchant"
-        )
-        result = merchant.transaction(config)
-        self.wfile.write(result.__dict__)
-        return
-    
-    def do_GET(self):
-    	parsed_path = urlparse.urlparse(self.path)
-    	amount, auth_token, currency, order_id = parsed_path.path.split("/")[1:]
-        result = self.pierTransaction(amount, auth_token, currency, order_id)
-        return
+@app.route("/<amount>/<auth_token>/<currency>/<id_in_merchant>/")
+def do_GET(amount, auth_token, currency, id_in_merchant):
+	return pierTransaction(amount, auth_token, currency, id_in_merchant)
 
-    def do_POST(self):
-        content_len = int(self.headers.getheader('content-length', 0))
-        postvars = json.loads(self.rfile.read(content_len))
-        amount, auth_token, currency, id_in_merchant = \
-            postvars["amount"], \
-            postvars["auth_token"], \
-            postvars["currency"], \
-            postvars["id_in_merchant"]
-        result = self.pierTransaction(amount, auth_token, currency, id_in_merchant)
-        return
-
-if __name__ == '__main__':
-    server = HTTPServer(('localhost', 8080), MerchantHandler)
-    print 'Starting server on port 8080...'
-    server.serve_forever()
+@app.route("/", methods=['POST'])
+def do_POST():
+	postvars = json.loads(request.data)
+	amount, auth_token, currency, id_in_merchant = \
+		postvars["amount"], \
+		postvars["auth_token"], \
+		postvars["currency"], \
+		postvars["id_in_merchant"]
+	return pierTransaction(amount, auth_token, currency, id_in_merchant)
+ 
+if __name__ == "__main__":
+    app.run()
