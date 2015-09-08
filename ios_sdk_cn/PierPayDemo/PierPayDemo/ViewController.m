b089ae8eb5f60856b4f2d16de6cee84216cf535c
zyma <zyma678@gmail.com>
2015-09-08 20:26:39 +0800

    iOS Fake Merchant.

--- ios_sdk_cn/PierPayDemo/PierPayDemo/ViewController.m
+++ ios_sdk_cn/PierPayDemo/PierPayDemo/ViewController.m
@@ -9,11 +9,29 @@
 #import "ViewController.h"
 #import "PushViewController.h"
 #import "RSADigitalSignature.h"
+#import "PierPaySDK.h"
+#import "RSADigitalSignature.h"
+#import "OrderUtil.h"
+
+@interface ProductCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *productLabel;
+@property (nonatomic, weak) IBOutlet UILabel *productDetailLabel;
+@property (nonatomic, weak) IBOutlet UILabel *salesLabel;
+@property (nonatomic, weak) IBOutlet UILabel *priceLabel;
+
+@property (nonatomic, weak) IBOutlet UIButton *payButton;
+@property (nonatomic, weak) IBOutlet UIImageView *productLogoImage;
+
+@end
 
 @interface ViewController ()
 
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *data;
+
+@property (nonatomic, strong) PierPaySDK *pierPay;
+
 
 @end
 
@@ -22,29 +40,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 90;
+    [self.tableView reloadData];
+    
     _data = @[@{@"title":@"PayByPier", @"detail":@"push"}, @{@"title":@"PayByPier", @"detail":@"model"}];
     
-    NSString *sign = [RSADigitalSignature createSignature:@"123" privateKey:@"MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAPbRrFFeG9ViAwYk"
-                      "BdeWel4BIVP3JjpQDjTBN3c63SWEa64J+iXGX/8/d3x6D9aVIiLzbwQ5afJ4DA4V"
-                      "wLjiIl09a9xs/X8F0BEWUxKoVXrARGVs1mB5HllnTzEXTOVEn+HjoQURNAuWqBGV"
-                      "INjmsLpJl8aMq5fbpSBzxzQC4x3vAgMBAAECgYBzQf6CELxWrOpUl8XSowaJl2WE"
-                      "3EkRugioQgIwv2A+ANR39VjHAxgZDf4yNp3mysWiJKOXCWicPcsDWM0iiRcaIKwt"
-                      "T4t3fvZ8DR6SFKht5JkjxJYMCwlFVeS2OQ+KOVcXlhWXjUO1V+3N/eXqiGGVeBl9"
-                      "mPADcqP4wgzT2SDzYQJBAPtkGNp4peMsv27siVh6JRwxbgAf55Homlisygw7h9Yi"
-                      "lJ/5XJABt7Lag2sk1rixY/oH3vhH7g0b4mSdJhEPqaUCQQD7WB4VRdoEtVyqW0Rk"
-                      "NnUVnGHhAOGfdh6f/+NMMvbnXkblqCV8c5XxOjdfQMO5hfair0aNJgcA1LLzUAPs"
-                      "f80DAkEApE0ylS8/NG/dmhDMX2BNetSvkTNI9SryHbyovT/3MrQdMUUYAyKsPh/k"
-                      "vpUwJTwDHLoiN2FDq5uq5ply9Lmo5QJAd5xTlKQNQLheROPyBA62YXZuTflxZcV8"
-                      "hX/s11JZlXmUG66NSFBpRscBmt7jReKuoHTxCjLSml6eWpP1ihK3qQJBALvInreQ"
-                      "dfTIAZS2oYYoE340ohHLbow4+uEOXnSkD2m95MqNg4ghda6fwAS7O+lIt5vEB4nV"
-                      "X/nCZkNpHjMXDPo="];
-    
-    BOOL check = [RSADigitalSignature verify:@"123" sign:sign publicKey:@"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD20axRXhvVYgMGJAXXlnpeASFT"
-                  "9yY6UA40wTd3Ot0lhGuuCfolxl//P3d8eg/WlSIi828EOWnyeAwOFcC44iJdPWvc"
-                  "bP1/BdARFlMSqFV6wERlbNZgeR5ZZ08xF0zlRJ/h46EFETQLlqgRlSDY5rC6SZfG"
-                  "jKuX26Ugc8c0AuMd7wIDAQAB"];
-    
     [self setTitle:@"首页"];
+}
+
+- (void)createProductList{
+    _data = @[
+              @{@"product" : @"Apple iPhone 6",
+                @"logo" : @"iPhone",
+                @"detail" : @"Apple iPhone 6 (A1586) 16GB 金色 移动联通电信4G手机。",
+                @"type" : @"手机",
+                @"price" : @"￥4888.00",
+                @"count" : @"1",
+                @"total" : @"￥4888.00",
+                @"sales" : @"月销量2700件"
+                }];
+}
+
+- (IBAction)payByPier:(id)sender{
+    NSDictionary *param = @{
+                            @"merchant_id":@"MC0000001409",
+                            @"api_id":@"1819957c-1a3f-11e5-ba25-3a22fd90d682",
+                            @"api_secret_key":@"mk-prod-18199475-1a3f-11e5-ba25-3a22fd90d682",
+                            @"amount":@"11.01",
+                            @"charset":@"UTF-8",
+                            @"order_id":@"fsdirwl24932130fs",
+                            @"return_url":@"/checkout/orderList",
+                            @"order_detail":@"",
+                            @"sign_type":@"RSA"
+                            };
+    
+    NSString *sign = [OrderUtil getSgin:param];
+    
+    NSDictionary *charge = @{
+                             @"merchant_id":@"MC0000001409",
+                             @"api_id":@"1819957c-1a3f-11e5-ba25-3a22fd90d682",
+                             @"api_secret_key":@"mk-prod-18199475-1a3f-11e5-ba25-3a22fd90d682",
+                             @"amount":@"11.01",
+                             @"charset":@"UTF-8",
+                             @"order_id":@"fsdirwl24932130fs",
+                             @"return_url":@"/checkout/orderList",
+                             @"order_detail":@"",
+                             @"sign_type":@"RSA",
+                             @"sign":sign
+                             };
+    
+    _pierPay = [[PierPaySDK alloc] init];
+    
+    [_pierPay createPayment:charge delegate:self fromScheme:@"" completion:^(NSDictionary *result, NSError *error) {
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -57,25 +107,26 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *MyIdentifier = @"Identifier";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
-    cell.textLabel.text = [_data[indexPath.row] objectForKey:@"title"];
-    cell.detailTextLabel.text =[_data[indexPath.row] objectForKey:@"detail"];
+    NSInteger row = indexPath.row;
+    static NSString *indetifier = @"ProductCell";
+    ProductCell *cell = [tableView dequeueReusableCellWithIdentifier:indetifier];
+//    [cell.productLabel setText:[_data[row] objectForKey:@"product"]];
+    
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    switch (indexPath.row) {
-        case 0:
-            [self performSegueWithIdentifier:@"push" sender:self];
-            break;
-        case 1:
-            [self performSegueWithIdentifier:@"modal" sender:self];
-            break;
-        default:
-            break;
-    }
+//    switch (indexPath.row) {
+//        case 0:
+//            [self performSegueWithIdentifier:@"push" sender:self];
+//            break;
+//        case 1:
+//            [self performSegueWithIdentifier:@"modal" sender:self];
+//            break;
+//        default:
+//            break;
+//    }
 }
 
  #pragma mark - Navigation
