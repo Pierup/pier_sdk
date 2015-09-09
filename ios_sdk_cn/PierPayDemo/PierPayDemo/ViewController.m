b7f896656a327dd0d52dc6382c1f3158b2cafcdb
zyma <zyma678@gmail.com>
2015-09-09 10:22:54 +0800

    iOS SDK.

--- ios_sdk_cn/PierPayDemo/PierPayDemo/ViewController.m
+++ ios_sdk_cn/PierPayDemo/PierPayDemo/ViewController.m
@@ -12,6 +12,7 @@
 #import "PierPaySDK.h"
 #import "RSADigitalSignature.h"
 #import "OrderUtil.h"
+#import <objc/Object.h>
 
 @interface ProductCell : UITableViewCell
 
@@ -42,7 +43,9 @@
 @interface ViewController ()
 
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
+
 @property (nonatomic, strong) NSArray *data;
+@property (nonatomic, strong) NSMutableDictionary *selectedIndexMap;
 
 @property (nonatomic, strong) PierPaySDK *pierPay;
 
@@ -59,13 +62,15 @@
     
     self.selectedCount = 0;
     
+    self.selectedIndexMap = [[NSMutableDictionary alloc] init];
+    
     [self setupView];
     
     [self getProductList];
     
     [self.tableView reloadData];
     
-    [self setTitle:@"首页"];
+    [self setTitle:@"商城"];
 }
 
 - (void)setupView{
@@ -80,6 +85,10 @@
 }
 
 - (IBAction)addToCar:(id)sender{
+    UIButton *addCarButton = sender;
+    NSIndexPath *index = objc_getAssociatedObject(addCarButton, @"index");
+    [self.selectedIndexMap setValue:self.data[index.row] forKey:[NSString stringWithFormat:@"%ld",index.row]];
+    [self setAddCarButtonEnable:NO sender:sender];
     self.selectedCount += 1;
     [self.batchPayButton setEnabled:YES];
     [self.batchPayButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
@@ -87,6 +96,11 @@
 }
 
 - (IBAction)payByPier:(id)sender{
+    UIButton *byButton = sender;
+    NSIndexPath *index = objc_getAssociatedObject(byButton, @"index");
+    [self.selectedIndexMap setValue:self.data[index.row] forKey:[NSString stringWithFormat:@"%ld",index.row]];
+    [self getOrderDetail];
+    
     NSDictionary *param = @{
                             @"merchant_id":@"MC0000001409",
                             @"api_id":@"1819957c-1a3f-11e5-ba25-3a22fd90d682",
@@ -126,6 +140,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - tools
+
+- (NSArray *)getOrderDetail{
+    NSMutableArray *orderDetail = [[NSMutableArray alloc] init];
+    NSArray *keys = self.selectedIndexMap.allKeys;
+    for (int i = 0; i < [keys count]; i++) {
+        [orderDetail addObject:[self.data objectAtIndex:[keys[i] integerValue]]];
+    }
+    return orderDetail;
+}
+
+#pragma mark - delegate
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return _data.count;
 }
@@ -139,7 +166,29 @@
     [cell.salesLabel setText:[_data[row] objectForKey:@"sales"]];
     [cell.priceLabel setText:[_data[row] objectForKey:@"price"]];
     [cell.productLogoImage setImage:[UIImage imageNamed:[_data[row] objectForKey:@"logo"]]];
+    [cell.shopCarButton setTag:indexPath.row];
+    objc_setAssociatedObject(cell.shopCarButton, @"index", indexPath, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(cell.payButton, @"index", indexPath, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    if ([self.selectedIndexMap objectForKey:[NSString stringWithFormat:@"%ld", row]] != nil) {
+        [self setAddCarButtonEnable:NO sender:cell.shopCarButton];
+    }else{
+        [self setAddCarButtonEnable:YES sender:cell.shopCarButton];
+    }
+    
     return cell;
+}
+
+- (void)setAddCarButtonEnable:(BOOL)enable sender:(id)sender{
+    UIButton *addButton = (UIButton *)sender;
+    if (!enable) {
+        [addButton setEnabled:NO];
+        [addButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [addButton.layer setBorderColor:[[UIColor grayColor] CGColor]];
+    }else{
+        [addButton setEnabled:YES];
+        [addButton setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+        [addButton.layer setBorderColor:[[UIColor orangeColor] CGColor]];
+    }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
